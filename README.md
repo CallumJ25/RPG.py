f8d59723319bffef0c89c25469b82880ecede686
Callum Johnson <cal.alexander.johnson@gmail.com>
2018-05-06 12:26:44 -0400

    Got rid of excess files, added main loop, added random Bobo text.

--- README.md
+++
@@ -1,2 +0,0 @@
-# RPG.py
-a Buddding in dev RPG
--- RPG.py
+++ RPG.py
@@ -1,8 +1,10 @@
 import time
 import random
 first=1
+list = ["face"]
 intro=["Hello!","Welcome to Callum's RPG!","A place where monsters reign,","but where heroes dwell and grow.","You my friend,","shall have to defeat the evil that lurks across the land.","The mighty and powerful,","Rowan.","It shall be difficult,","but I sense something special about you."]
-Bobo_intro=["???: *grumble* Fine, come on in.", "???: Just don't ask me for money for your little club or somthing.","???: And we don't have candy either.", "???: Oh! Sorry, I didn't know that an adventurer would come here!","???: My name is Bobo, the humble owner of this shop.","Bobo: We have many things for people like you.","Bobo: Magic tomes, swords, shields, and more!","Bobo: Just make sure you have enough money, and anything is yours."]
+Bobo_intro=["???: *grumble* Fine, come on in.", "???: Just don't ask me for money for your little club or somthing.","???: And we don't have candy either.", "???: Oh! Sorry, I didn't know that an adventurer would come here!","Bobo: My name is Bobo, the humble owner of this shop.","Bobo: We have many things for people like you.","Bobo: Magic tomes, swords, shields, and more!","Bobo: Just make sure you have enough money, and anything is yours."]
+Bobo=["Bobo: Hello there!","Bobo: No monsters here, eh?","Bobo: Welcome to Bobo's Armory!"]
 for s in intro:
     print s
     time.sleep(2)
@@ -10,7 +12,7 @@ class Game:
  def __init__(self, HP, ATK):
   self.HP = HP
   self.ATK = ATK
-player = Game("21", "29")
+player = Game("20", "5")
 exp=110
 level=1
 level_req=110
@@ -24,10 +26,17 @@ def level_up():
         level=level+1
         exp = exp - level_req
         level_req = level * 100 + level * 10
-input = raw_input("Where shall you go? ")
-if input == "shop":
-    if first == 1:
-        for i in Bobo_intro:
-            print i
-            time.sleep(1.8)
-
+def NPC_text():
+    global list
+    print list[int((random.uniform(0,3)))]
+    time.sleep(1)
+while True:
+    input = raw_input("Where shall you go? ")
+    if input == "shop":
+        if first == 1:
+            for i in Bobo_intro:
+                print i
+                time.sleep(1.8)
+            first=0
+    list=Bobo
+    NPC_text()
